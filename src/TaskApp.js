c5ef8253852db40f1d348ee870b138ac42e2c075
Davidhernandezn <d4vidhernandezn@gmail.com>
2023-12-08 17:19:35 -0600

    Inicio de desarrollo, estructura de task appp

---
+++ src/TaskApp.js
@@ -0,0 +1,18 @@
+import { html, css, LitElement } from 'lit';
+
+export class TaskApp extends LitElement {
+
+  static properties = {
+  };
+
+  constructor() {
+    super();
+  }
+
+
+  render() {
+    return html`
+      <h2>Task App</h2>
+    `;
+  }
+}
---
+++ task-app.js
@@ -0,0 +1,3 @@
+import { TaskApp } from './src/TaskApp.js';
+
+window.customElements.define('task-app', TaskApp);
